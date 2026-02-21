d0f81bbe6a91be5833d81246b005f7534699c2fb
jhanryw <wjhanry@gmail.com>
2026-02-21 09:01:47 -0300

    limpeza: reset de código e fix definitivo de callback

--- src/lib/auth/logto.ts
+++ src/lib/auth/logto.ts
@@ -2,8 +2,8 @@ import { UserScope, LogtoNextConfig } from '@logto/next';
 
 export const logtoConfig: LogtoNextConfig = {
     endpoint: (process.env.LOGTO_ENDPOINT || '').replace(/\/oidc$/, ''),
-    appId: process.env.LOGTO_APP_ID || '',
-    appSecret: process.env.LOGTO_APP_SECRET || '',
+    appId: process.env.LOGTO_APP_ID || '8xqcwaa3648zgjk6hwbkr',
+    appSecret: process.env.LOGTO_APP_SECRET || '8t5XBEK3K66WiYA3rPqdGL5Rc5kJrII8',
     baseUrl: (process.env.NEXT_PUBLIC_APP_URL || 'https://crm-crm.vodct5.easypanel.host/').replace(/\/$/, ''),
     cookieSecret: process.env.LOGTO_COOKIE_SECRET || 'complex_password_at_least_32_characters_long_cookie_secret',
     cookieSecure: process.env.NODE_ENV === 'production',
