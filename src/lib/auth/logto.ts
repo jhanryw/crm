d628fd49fa5694452bee4f02c4b86ce3db9d1301
jhanryw <wjhanry@gmail.com>
2026-02-21 07:52:15 -0300

    fix: removendo fallbacks do logto

--- src/lib/auth/logto.ts
+++ src/lib/auth/logto.ts
@@ -1,9 +1,9 @@
 import { UserScope, LogtoNextConfig } from '@logto/next';
 
 export const logtoConfig: LogtoNextConfig = {
-    endpoint: process.env.LOGTO_ENDPOINT || 'https://logto.dev',
-    appId: process.env.LOGTO_APP_ID || 'missing_app_id',
-    appSecret: process.env.LOGTO_APP_SECRET || 'missing_app_secret',
+    endpoint: process.env.LOGTO_ENDPOINT!,
+    appId: process.env.LOGTO_APP_ID!,
+    appSecret: process.env.LOGTO_APP_SECRET!,
     baseUrl: process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000',
     cookieSecret: process.env.LOGTO_COOKIE_SECRET || 'complex_password_at_least_32_characters_long',
     cookieSecure: process.env.NODE_ENV === 'production',
