2d0d5ef476f48ad6e2fda7e03b1e847b2f75fcfe
jhanryw <wjhanry@gmail.com>
2026-02-21 08:53:42 -0300

    fix: removendo /oidc do endpoint para teste

--- src/lib/auth/logto.ts
+++ src/lib/auth/logto.ts
@@ -5,10 +5,10 @@ if (!process.env.LOGTO_APP_ID) console.error('CRITICAL: LOGTO_APP_ID is missing!
 if (!process.env.NEXT_PUBLIC_APP_URL) console.error('CRITICAL: NEXT_PUBLIC_APP_URL is missing!');
 
 export const logtoConfig: LogtoNextConfig = {
-    endpoint: process.env.LOGTO_ENDPOINT || 'ERROR_MISSING_ENDPOINT',
+    endpoint: (process.env.LOGTO_ENDPOINT || 'https://logto.dev').replace(/\/oidc$/, ''),
     appId: process.env.LOGTO_APP_ID || 'ERROR_MISSING_APP_ID',
     appSecret: process.env.LOGTO_APP_SECRET || 'ERROR_MISSING_SECRET',
-    baseUrl: (process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000').replace(/\/$/, ''),
+    baseUrl: (process.env.NEXT_PUBLIC_APP_URL || 'https://crm-crm.vodct5.easypanel.host/').replace(/\/$/, ''),
     cookieSecret: process.env.LOGTO_COOKIE_SECRET || 'complex_password_at_least_32_characters_long',
     cookieSecure: process.env.NODE_ENV === 'production',
     scopes: [UserScope.Email, UserScope.Profile, UserScope.Roles],
